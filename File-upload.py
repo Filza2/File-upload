93cacd148dd9b144dad9c64e255f713ebd9bc8bb
TweakPY <69265406+Filza2@users.noreply.github.com>
2023-03-24 06:44:10 +0300

    Update File-upload.py

--- File-upload.py
+++ File-upload.py
@@ -1,6 +1,7 @@
 from requests import post
-from uuid import uuid4
 import os
+
+
 def header():
     os.system("cls" if os.name=='nt' else "clear");print("""
 ███████╗██╗██╗     ███████╗    ██╗   ██╗██████╗ ██╗      ██████╗  █████╗ ██████╗ 
@@ -12,19 +13,18 @@ def header():
                                                                                   
                             By @TweakPY - @vv1ck
 """)
-def File_uploader():
-    content_file=input("- The File Name : ")
-    file_name=uuid4()
-    try:file_content=open(content_file,'r').read()
-    except FileNotFoundError:exit('- File Not Found ! ')
-    d=f'''-----------------------------240332332236763587692188904424
-Content-Disposition: form-data; name="file"; filename="{file_name}"
-Content-Type: application/octet-stream
-
-{file_content}
------------------------------240332332236763587692188904424--'''
-    r=post('https://ttm.sh/',headers={'Host': 'ttm.sh','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Content-Type': 'multipart/form-data; boundary=---------------------------240332332236763587692188904424'},data=d)
-    if "https://ttm.sh/" in r.text:header();print(f"- Done successfully, Your File at : {r.text}")
-    else:header();exit("- Error ! ")#print(r.text)
     
+    
+def File_uploader():
+    try:r=post("https://api.anonfiles.com/upload",files={'file':open(input("- The File Name : "),"rb")});header()
+    except FileNotFoundError:exit('- File Not Found ! \n')
+    if "full" in r.text and 'short' in r.text and 'id' in r.text:print(f'- Done successfully, Your File at : {r.json()["data"]["file"]["url"]["short"]}\n')
+    elif 'error' in r.text:
+        try:exit(f"- Error, {r.json()['error']['message']}\n")
+        except Exception as e:exit("- Error !\n")
+    else:exit("- Error !\n")
+    
+    
+    
+
 header();File_uploader()
